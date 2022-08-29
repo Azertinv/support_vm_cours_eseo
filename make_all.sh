29ae24f8413d91869c10053224153c715bfc1f9a
Azertinv <azertinv@protonmail.com>
2022-08-29 15:16:32 +0200

    forget to add fixme chall to make all script

--- make_all.sh
+++ make_all.sh
@@ -2,7 +2,10 @@
 
 make -C chall/exploit/00_ret_into_system "$@"
 make -C chall/exploit/01_ret_into_shellcode "$@"
+
 make -C chall/reverse/00_basic_if "$@"
 make -C chall/reverse/01_basic_strcmp "$@"
 make -C chall/reverse/02_multiple_ifs "$@"
+
+make -C chall/fixme/java_int_overflow "$@"
 
